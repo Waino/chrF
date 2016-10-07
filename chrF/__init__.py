5cf87c858296d42844a0b2d92ff0095ebf7ec0c9
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2016-10-07 16:50:19 +0300

    ngrams, errors

---
+++ chrF/__init__.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+chrF - Reimplementation of the character-F evaluation measure for SMT
+Original by Maja Popovic
+"""
+
+import collections
+
+#__all__ = []
+
+__version__ = '0.0.1'
+__author__ = 'Stig-Arne Gronroos'
+__author_email__ = "stig-arne.gronroos@aalto.fi"
+
+#_logger = logging.getLogger(__name__)
+
+
+def ngrams(line, max_n, use_space=True):
+    """Yields all character n-grams of lengths from 1 to max_n.
+    If use_space is False, spaces are not counted as chars."""
+    # FIXME: need to group by n
+    if not use_space:
+        line = line.replace(' ', '')
+    max_n = min(max_n, len(line))
+    for n in range(1, max_n + 1):
+        offsets = [line[i:] for i in range(n)]
+        for ngram in zip(*offsets):
+            yield ngram
+
+
+def errors(hypothesis, reference):
+    errorcount = 0.0
+    precrec = 0.0
+    missing = []
+
+    ref_counts = collections.Counter(reference)
+    for ngram in hypothesis:
+        if ref_counts[ngram] > 0:
+            ref_counts[ngram] -= 1
+        else:
+            errorcount += 1
+            missing.append(ngram)
+
+        if len(hypothesis) != 0:
+            precrec = 100 * errorcount / len(hypothesis)
+        else:
+            if len(reference) != 0:
+                precrec = 100
+            else:
+                precrec = 0
+
+    return errorcount, precrec, missing
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+import re
+main_py = open('chrF/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
+
+#requires = [ ]
+
+setup(name='chrF',
+      version=metadata['version'],
+      author=metadata['author'],
+      author_email='stig-arne.gronroos@aalto.fi',
+      #url='',
+      description='chrF',
+      packages=['chrF', 'chrF.tests'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          #'Topic :: Scientific/Engineering',
+      ],
+      license="BSD",
+      #scripts=['scripts/chrF',
+      #        ],
+      #install_requires=requires,
+      #extras_require={
+      #    'docs': [l.strip() for l in open('docs/build_requirements.txt')]
+      #}
+      )
